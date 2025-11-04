995415bb37849851a508ca24622c5ecbb779ab18
ar4 <9070970@gmail.com>
2025-11-04 17:03:15 +0300

    Next commit

---
+++ nano nano-scopatz.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+
+USER=ar4
+sudo -v
+
+
+mkdir -p /home/$USER/.config/nano
+cp /etc/nanorc /home/$USER/.config/nano/nanorc
+
+sudo mkdir -p /root/.config/nano
+sudo cp /etc/nanorc /root/.config/nano/nanorc
+
+
+curl https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | sh
+
+sudo cp -r /home/$USER/.nano /root/ 
+
+
+# Исправляем ошибку в nanorc.nanorc
+sudo sed -i 's/^icolor brightnormal " brightnormal"/# &/' /root/.nano/nanorc.nanorc
+
+
+cat <<EOF>> /home/$USER/.config/nano/nanorc 
+#
+#
+set indicator
+set historylog
+set locking
+set mouse
+#set linenumbers
+
+# Указываем темы для подсветки синтаксиса всевозможных фрматов
+include "/home/$USER/.nano/*.nanorc"
+EOF
+
+
+sudo tee -a /root/.config/nano/nanorc <<EOF
+#
+#
+set indicator
+set historylog
+set locking
+set mouse
+#set linenumbers
+
+# Указываем темы для подсветки синтаксиса всевозможных форматов
+include "/root/.nano/*.nanorc"
+EOF
